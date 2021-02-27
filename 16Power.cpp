134c208ca3f90d53a9fc1edac71fe040b39743e4
mext169 <1922077535@qq.com>
2021-02-27 21:42:55 +0800

    add 16

---
+++ 16Power.cpp
@@ -0,0 +1,137 @@
+//
+// Created by mext on 2021/2/27.
+//
+/*******************************************************************
+Copyright(c) 2016, Harry He
+All rights reserved.
+
+Distributed under the BSD license.
+(See accompanying file LICENSE.txt at
+https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
+*******************************************************************/
+
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题16：数值的整数次方
+// 题目：实现函数double Power(double base, int exponent)，求base的exponent
+// 次方。不得使用库函数，同时不需要考虑大数问题。
+
+#include <iostream>
+#include <cmath>
+
+bool g_InvalidInput = false;
+bool equal(double num1, double num2);
+double PowerWithUnsignedExponent(double base, unsigned int exponent);
+
+double Power(double base, int exponent)
+{
+    // 表示输入是否有效的全局flag
+    g_InvalidInput = false;
+
+    // 0的负指数次幂没有意义，这里返回0.0表示
+    if (equal(base, 0.0) && exponent < 0)
+    {
+        g_InvalidInput = true;
+        return 0.0;
+    }
+
+    unsigned int absExponent = (unsigned int) (exponent);
+    if (exponent < 0)
+        absExponent = (unsigned int) (-exponent);
+
+    double result = PowerWithUnsignedExponent(base, absExponent);
+    if (exponent < 0)
+        result = 1.0 / result;
+
+    return result;
+}
+
+/*
+double PowerWithUnsignedExponent(double base, unsigned int exponent)
+{
+    double result = 1.0;
+
+    for (int i = 1; i <= exponent; ++i)
+        result *= base;
+
+    return result;
+}
+*/
+
+/**
+ * 递归起来了，算a^8 只需算a^2, a^4, 等
+ * @param base
+ * @param exponent
+ * @return
+ */
+double PowerWithUnsignedExponent(double base, unsigned int exponent)
+{
+    if (exponent == 0)
+        return 1;
+    if (exponent == 1)
+        return base;
+
+    // 右移代替除以2
+    double result = PowerWithUnsignedExponent(base, exponent >> 1);
+    result *= result;
+    // 用与运算判断奇数偶数
+    if ((exponent & 0x1) == 1)
+        result *= base;
+
+    return result;
+}
+
+/**
+ * 判断两个double型数字是否相等，注意不能直接==
+ * @param num1
+ * @param num2
+ * @return
+ */
+bool equal(double num1, double num2)
+{
+    if ((num1 - num2 > -0.0000001) && (num1 - num2 < 0.0000001))
+        return true;
+    else
+        return false;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, double base, int exponent, double expectedResult, bool expectedFlag)
+{
+    double result = Power(base, exponent);
+    if (equal(result, expectedResult) && g_InvalidInput == expectedFlag)
+        std::cout << testName << " passed" << std::endl;
+    else
+        std::cout << testName << " FAILED" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    // 底数、指数都为正数
+    Test("Test1", 2, 3, 8, false);
+
+    // 底数为负数、指数为正数
+    Test("Test2", -2, 3, -8, false);
+
+    // 指数为负数
+    Test("Test3", 2, -3, 0.125, false);
+
+    // 指数为0
+    Test("Test4", 2, 0, 1, false);
+
+    // 底数、指数都为0
+    Test("Test5", 0, 0, 1, false);
+
+    // 底数为0、指数为正数
+    Test("Test6", 0, 4, 0, false);
+
+    // 底数为0、指数为负数
+    Test("Test7", 0, -4, 0, true);
+
+    return 0;
+}
+
+
