caf86e89b26a2a4f9a5fe3ee1971c1b92167d979
mext169 <1922077535@qq.com>
2021-03-10 00:11:06 +0800

    add 22/23

---
+++ 22KthNodeFromEnd.cpp
@@ -0,0 +1,198 @@
+//
+// Created by mext on 2021/3/9.
+//
+
+/*******************************************************************
+Copyright(c) 2016, Harry He
+All rights reserved.
+
+Distributed under the BSD license.
+(See accompanying file LICENSE.txt at
+https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
+*******************************************************************/
+
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题22：链表中倒数第k个结点
+// 题目：输入一个链表，输出该链表中倒数第k个结点。为了符合大多数人的习惯，
+// 本题n从1开始计数，即链表的尾结点是倒数第1个结点。例如一个链表有6个结点，
+// 从头结点开始它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个结点是
+// 值为4的结点。
+
+// 思路：
+// 假设链表长度为n，倒数第k个节点即为正数第n-k+1个节点
+// 因此需要首先知道链表有多少个节点
+// 1. 遍历链表的所有节点，然后用计数器统计节点的个数，之后再找到第n-k+1个节点。这个方法需要遍历两次链表，不够高效
+// 2. 使用两个指针，第一个指针从头开始遍历走k-1步后，第二个指针从头开始与第一个指针一起向前遍历，当第一个到达链表的末尾时，
+//    第二个指针刚好达到倒数第k个节点。因此两个指针相距k-1.
+
+#include <cstdio>
+#include "Utilities/List.h"
+
+ListNode* FindKthToTail(ListNode* pListHead, unsigned int k)
+{
+    // 输入检测
+    if(pListHead == nullptr || k == 0)
+        return nullptr;
+
+    ListNode *pAhead = pListHead;
+    ListNode *pBehind = nullptr;
+
+    // A指针向前走k-1
+    for(unsigned int i = 0; i < k - 1; ++ i)
+    {
+        // 防止k>n出现
+        if(pAhead->m_pNext != nullptr)
+            pAhead = pAhead->m_pNext;
+        else
+        {
+            return nullptr;
+        }
+    }
+
+    pBehind = pListHead;
+    // A和B同时向前走，知道A到达链表的末尾
+    while(pAhead->m_pNext != nullptr)
+    {
+        pAhead = pAhead->m_pNext;
+        pBehind = pBehind->m_pNext;
+    }
+
+    return pBehind;
+}
+
+// ====================测试代码====================
+// 测试要找的结点在链表中间
+void Test1()
+{
+    printf("=====Test1 starts:=====\n");
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    printf("expected result: 4.\n");
+    ListNode* pNode = FindKthToTail(pNode1, 2);
+    PrintListNode(pNode);
+
+    DestroyList(pNode1);
+}
+
+// 测试要找的结点是链表的尾结点
+void Test2()
+{
+    printf("=====Test2 starts:=====\n");
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    printf("expected result: 5.\n");
+    ListNode* pNode = FindKthToTail(pNode1, 1);
+    PrintListNode(pNode);
+
+    DestroyList(pNode1);
+}
+
+// 测试要找的结点是链表的头结点
+void Test3()
+{
+    printf("=====Test3 starts:=====\n");
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    printf("expected result: 1.\n");
+    ListNode* pNode = FindKthToTail(pNode1, 5);
+    PrintListNode(pNode);
+
+    DestroyList(pNode1);
+}
+
+// 测试空链表
+void Test4()
+{
+    printf("=====Test4 starts:=====\n");
+    printf("expected result: nullptr.\n");
+    ListNode* pNode = FindKthToTail(nullptr, 100);
+    PrintListNode(pNode);
+}
+
+// 测试输入的第二个参数大于链表的结点总数
+void Test5()
+{
+    printf("=====Test5 starts:=====\n");
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    printf("expected result: nullptr.\n");
+    ListNode* pNode = FindKthToTail(pNode1, 6);
+    PrintListNode(pNode);
+
+    DestroyList(pNode1);
+}
+
+// 测试输入的第二个参数为0
+void Test6()
+{
+    printf("=====Test6 starts:=====\n");
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    printf("expected result: nullptr.\n");
+    ListNode* pNode = FindKthToTail(pNode1, 0);
+    PrintListNode(pNode);
+
+    DestroyList(pNode1);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+
+    return 0;
+}
+
---
+++ 23EntryNodeInListLoop.cpp
@@ -0,0 +1,251 @@
+//
+// Created by mext on 2021/3/9.
+//
+
+/*******************************************************************
+Copyright(c) 2016, Harry He
+All rights reserved.
+
+Distributed under the BSD license.
+(See accompanying file LICENSE.txt at
+https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
+*******************************************************************/
+
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题23：链表中环的入口结点
+// 题目：一个链表中包含环，如何找出环的入口结点？例如，在图3.8的链表中，
+// 环的入口结点是结点3。
+
+// 1. 确定一个链表中是否包含环
+//    两个指针从链表头出发，一个一次一步，一个一次两步，如果走的快的追上了走的慢的，那么链表就包含环
+// 2. 找到环的入口
+//    两个指针，指向头节点，如果环中有n个节点，那么先让P1前移n个节点，然后两个指针以相同的速度移动，当第二个指针指向环的入口时，
+//    第一个指针已经围绕着环走了一圈
+// 3. 环中节点的数目
+//    一快一慢两个指针，一定相遇在环中，从这个节点出发，一边向前移动一边计数，当再次回到这个节点时，就可以得到环中的节点数了
+
+#include <cstdio>
+#include "Utilities/List.h"
+
+ListNode* MeetingNode(ListNode* pHead)
+{
+    if(pHead == nullptr)
+        return nullptr;
+
+    ListNode* pSlow = pHead->m_pNext;
+    if(pSlow == nullptr)
+        return nullptr;
+
+    ListNode* pFast = pSlow->m_pNext;
+    while(pFast != nullptr && pSlow != nullptr)
+    {
+        if(pFast == pSlow)
+            return pFast;
+
+        // 慢的一次走一个
+        pSlow = pSlow->m_pNext;
+
+       // 快的一次走两个，要判断一下快的下一个不为空指针
+        pFast = pFast->m_pNext;
+        if(pFast != nullptr)
+            pFast = pFast->m_pNext;
+    }
+
+    return nullptr;
+}
+
+ListNode* EntryNodeOfLoop(ListNode* pHead)
+{
+    // 快的指针和慢的指针在环中的某一个节点相遇
+    ListNode* meetingNode = MeetingNode(pHead);
+    if(meetingNode == nullptr)
+        return nullptr;
+
+    // 得到环中结点的数目
+    int nodesInLoop = 1;
+    ListNode* pNode1 = meetingNode;
+    while(pNode1->m_pNext != meetingNode)
+    {
+        pNode1 = pNode1->m_pNext;
+        ++nodesInLoop;
+    }
+
+    // 先移动pNode1，次数为环中结点的数目
+    pNode1 = pHead;
+    for(int i = 0; i < nodesInLoop; ++i)
+        pNode1 = pNode1->m_pNext;
+
+    // 再移动pNode1和pNode2
+    ListNode* pNode2 = pHead;
+    while(pNode1 != pNode2)
+    {
+        pNode1 = pNode1->m_pNext;
+        pNode2 = pNode2->m_pNext;
+    }
+
+    return pNode1;
+}
+
+// ==================== Test Code ====================
+void Test(char* testName, ListNode* pHead, ListNode* entryNode)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    if(EntryNodeOfLoop(pHead) == entryNode)
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+// A list has a node, without a loop
+void Test1()
+{
+    ListNode* pNode1 = CreateListNode(1);
+
+    Test("Test1", pNode1, nullptr);
+
+    DestroyList(pNode1);
+}
+
+// A list has a node, with a loop
+void Test2()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ConnectListNodes(pNode1, pNode1);
+
+    Test("Test2", pNode1, pNode1);
+
+    delete pNode1;
+    pNode1 = nullptr;
+}
+
+// A list has multiple nodes, with a loop
+void Test3()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode3);
+
+    Test("Test3", pNode1, pNode3);
+
+    delete pNode1;
+    pNode1 = nullptr;
+    delete pNode2;
+    pNode2 = nullptr;
+    delete pNode3;
+    pNode3 = nullptr;
+    delete pNode4;
+    pNode4 = nullptr;
+    delete pNode5;
+    pNode5 = nullptr;
+}
+
+// A list has multiple nodes, with a loop
+void Test4()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode1);
+
+    Test("Test4", pNode1, pNode1);
+
+    delete pNode1;
+    pNode1 = nullptr;
+    delete pNode2;
+    pNode2 = nullptr;
+    delete pNode3;
+    pNode3 = nullptr;
+    delete pNode4;
+    pNode4 = nullptr;
+    delete pNode5;
+    pNode5 = nullptr;
+}
+
+// A list has multiple nodes, with a loop
+void Test5()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode5);
+
+    Test("Test5", pNode1, pNode5);
+
+    delete pNode1;
+    pNode1 = nullptr;
+    delete pNode2;
+    pNode2 = nullptr;
+    delete pNode3;
+    pNode3 = nullptr;
+    delete pNode4;
+    pNode4 = nullptr;
+    delete pNode5;
+    pNode5 = nullptr;
+}
+
+// A list has multiple nodes, without a loop
+void Test6()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    Test("Test6", pNode1, nullptr);
+
+    DestroyList(pNode1);
+}
+
+// Empty list
+void Test7()
+{
+    Test("Test7", nullptr, nullptr);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+    return 0;
+}
+
