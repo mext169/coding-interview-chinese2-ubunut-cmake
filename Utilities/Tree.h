0e72d599bfe474a80e9c0a29a74b671e2e1d31ad
mext169 <1922077535@qq.com>
2021-04-05 16:12:01 +0800

    add 67

---
+++ Utilities/Tree.h
@@ -0,0 +1,28 @@
+//
+// Created by lab on 2021/4/5.
+//
+
+/*******************************************************************
+Copyright(c) 2016, Harry He
+All rights reserved.
+
+Distributed under the BSD license.
+(See accompanying file LICENSE.txt at
+https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
+*******************************************************************/
+
+#pragma once
+
+#include <vector>
+
+struct TreeNode
+{
+    int                       m_nValue;
+    std::vector<TreeNode*>    m_vChildren;
+};
+
+TreeNode* CreateTreeNode(int value);
+void ConnectTreeNodes(TreeNode* pParent, TreeNode* pChild);
+void PrintTreeNode(const TreeNode* pNode);
+void PrintTree(const TreeNode* pRoot);
+void DestroyTree(TreeNode* pRoot);
